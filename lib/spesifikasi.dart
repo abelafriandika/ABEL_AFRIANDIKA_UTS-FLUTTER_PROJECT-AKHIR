3f0d338d25e914df514a68c8b38af6619ed12047
abelafriandika <abel258karseno@gmail.com>
2023-07-22 00:37:27 +0700

    memperbaiki codingan

--- lib/spesifikasi.dart
+++ lib/spesifikasi.dart
@@ -13,61 +13,37 @@ List<spesifikasi> dataspesifikasi = [
       name: 'iphone11',
       price: '9.6 juta',
       spec: '1. hp terkini 2. idaman para kaum remaja 3. ramah dikantong.',
-<<<<<<< HEAD
       image: 'assets/images/iphone.jpeg'),
-=======
-      image: '/images/iphone.jpeg'),
->>>>>>> d3450435c8fec1630476f74d5fa52a8d597828af
   spesifikasi(
       name: 'iphone13.',
       price: '17.9 juta',
       spec: '1. hp rekomend 2. flagship dari apple ',
-<<<<<<< HEAD
       image: 'assets/images/iphone13.jpeg'),
-=======
-      image: '/images/iphone13.jpeg'),
->>>>>>> d3450435c8fec1630476f74d5fa52a8d597828af
   spesifikasi(
       name: 'samsungs23ultra',
       price: '18.99 juta',
       spec: '1. Hp Flagship terbaru samsung',
-<<<<<<< HEAD
       image: 'assets/images/Samsung_s23_ultra.jpeg'),
-=======
-      image: '/images/Samsung_s23_ultra.jpeg'),
->>>>>>> d3450435c8fec1630476f74d5fa52a8d597828af
   spesifikasi(
       name: 'ROGphone2',
       price: '8 juta ',
       spec: '1. Hp Gaming dari Brand Asus',
-<<<<<<< HEAD
       image: 'assets/images/ROG_PHONE_2.jpeg'),
-=======
-      image: '/images/ROG_PHONE_2.jpeg'),
->>>>>>> d3450435c8fec1630476f74d5fa52a8d597828af
   spesifikasi(
       name: 'iphone14 pro',
       price: '17 juta ',
       spec: '1. Hp terbaru keluaran brand apple',
-<<<<<<< HEAD
       image: 'assets/images/iphone-14-pro.jpeg'),
-=======
-      image: 'images/iphone-14-pro.jpeg'),
->>>>>>> d3450435c8fec1630476f74d5fa52a8d597828af
   spesifikasi(
       name: 'laptop asus rog513ih',
       price: '19 juta ',
       spec: '1. laptop gaming keluaran brand asus ',
-<<<<<<< HEAD
       image: 'assets/images/rog-g513-ih.jpeg'),
-=======
-      image: 'images/rog-g513-ih.jpeg'),
->>>>>>> d3450435c8fec1630476f74d5fa52a8d597828af
   spesifikasi(
       name: 'laptop asus tuf A15',
       price: '15 juta ',
       spec: '1. laptop gaming keluaran brand asus 2.ramah dikantong ',
-      image: 'images/asus-tuf-15.jpeg'),
+      image: 'assets/images/asus-tuf-15.jpeg'),
   spesifikasi(
     name: 'redmi note 8' ,
     price: ' 3 juta ',
